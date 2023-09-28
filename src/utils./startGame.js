9bb136e8d994f2c7eb47596968da8a65eec3aa64
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-09-28 22:48:01 +0200

    startGame returns non repeating numbers now

--- src/utils./startGame.js
+++ src/utils./startGame.js
@@ -1,13 +1,14 @@
-const startGame = (numOfButtons,howManyButtonsShouldBePicked) => {
-    console.log(howManyButtonsShouldBePicked)
-    const drawnNumbers = [];
-    for (let i = 0; i < howManyButtonsShouldBePicked; i++) {
-      
-      const drawnNumber = Math.floor(Math.random() * numOfButtons)+1;
-      drawnNumbers.push(drawnNumber)
-      
-    }
-    console.log(drawnNumbers)
-  };
+const startGame = (numOfButtons, howManyButtonsShouldBePicked) => {
+  let bank = Array.from({ length: 45 }, (_, i) => i + 1);
+  let drawnNumbers = [];
 
-  export default startGame;
+  for (let i = 0; i < howManyButtonsShouldBePicked; i++) {
+    const indexBankArr = Math.floor(Math.random() * bank.length);
+    let drawnNumber = bank[indexBankArr];
+    bank.splice(indexBankArr, 1);
+    drawnNumbers.push(drawnNumber);
+  }
+  console.log(drawnNumbers, bank.length);
+};
+
+export default startGame;
